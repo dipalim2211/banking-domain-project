1d309c2c8ed117d82d0472b6c1a6132801e75a52
dipalim2211 <dipalimanere22@gmail.com>
2025-03-19 13:29:49 +0530

    UseCase 01 Code check

--- banking-services/src/main/java/com/devgen/banking/BankingCustomerClient.java
+++ banking-services/src/main/java/com/devgen/banking/BankingCustomerClient.java
@@ -7,6 +7,9 @@ import com.devgen.banking.model.SavingAccount;
 
 public class BankingCustomerClient {
 
+
+    public static void main(String[] args) {
+
     /* Account Abstract Class
     Account account = new Account(12345,1000, AccountType.CURRENT);
 
@@ -16,8 +19,21 @@ public class BankingCustomerClient {
     CurrentAccount c1 = new CurrentAccount(2345,3000);
      */
 
-    Account s1 = new SavingAccount(13456,2000);
-    Account c1 = new CurrentAccount(2345,3000);
+
+        //Test UseCase 01:
+        Account s1 = new SavingAccount(13456, 2000);
+
+        System.out.println("Account Number : "+s1.getAccountNumber()+ " "+"" +
+                "Balance : "+s1.getBalance());
+
+        Account c1 = new CurrentAccount(23453, 3000);
 
 
+        System.out.println("Account Number : "+c1.getAccountNumber()+ " "+"" +
+                "Balance : "+c1.getBalance());
+
+
+
+
+    }
 }
